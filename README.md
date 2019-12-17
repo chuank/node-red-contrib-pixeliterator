e835ddc417cb324b115529371200cc5829559ff4
Chuan Khoo <chuank@users.noreply.github.com>
2019-12-17 21:39:28 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+node-red-contrib-pixeliterator
+==============================
+
+<a href="https://nodered.org" target="_blank">Node-RED</a> node to retrieve photos and metadata from Instagram.
+
+Install
+-------
+
+Run the following command in your ~/.node-red directory
+
+        npm install chuank/node-red-contrib-pixeliterator
+
+
+Usage
+-----
+
+Takes an image url, loads the image and generates an array of pixel RGB values
+A single node to retrieve photos from Instagram. Set a timed interval or send a message to it to trigger retrieval.
+
+You can choose from 1-20 of the latest images to retrieve from the authenticated IG user.
+
+Send the IG media's ID as input to retrieve information about the image (with the option to request the image sent as a Buffer).
+
+
+### Note:
+
+Videos are currently not supported and are ignored.
---
+++ pixeliterator.js
@@ -0,0 +1,50 @@
+/**
+ *
+ *  node-red-contrib-pixeliterator
+ *  Copyright 2019 Chuan Khoo.
+ *  www.chuank.com
+ *
+ **/
+module.exports = function(RED) {
+	"use strict";
+	var Jimp = require("jimp");
+
+	function PixelIteratorNode(n) {
+		RED.nodes.createNode(this,n);
+
+		var node = this;
+		node.pollInterval = n.pollinterval;
+
+		node.instagramConfig = RED.nodes.getNode(n.instagram);
+		if (!node.instagramConfig) {
+			node.warn(RED._("instagram.warn.missing-credentials"));
+			return;
+		}
+
+		node.on("input", function(msg) {
+			// process incoming data; allows either string url, or Buffer containing raw image data (KIV)
+
+			// v0.1.0: doing just string URL for now
+			readImage(node, msg.payload);
+		});
+
+		node.on("close", function() {		// tidy up!
+			// clearInterval(node.refreshTokenIntervalID);			//long-lived token refresh (15min)
+			// delete node.refreshTokenIntervalID;
+		});
+	}
+
+	function readImage(node, url) {
+		Jimp.read(url)
+			.then(function(image) {
+				// Do stuff with the image.
+				node.trace("jimp image:");
+				node.trace(image.width, ",", image.height);
+			})
+			.catch(function(err) {
+				node.error(err);
+			});
+	}
+
+	RED.nodes.registerType("pixeliterator", PixelIteratorNode);
+};
